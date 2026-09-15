4b3a8fbb99d68c09c4feea83c615e70aee1fbe4e
cyrillotk <tatacyri1l@gmail.com>
2026-09-15 05:07:02 +0100

    first commit

---
+++ src/app.ts
@@ -0,0 +1,20 @@
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (_req, res) => {
+  res.json({
+    message: "Task Management API is running"
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
